1431035dd442aed64e89ffcda8eab81884fcc912
mohammad dehdar <dehdardevelop@gmail.com>
2025-01-06 18:25:08 +0100

    Fix file system path for Vercel

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,14 +1,15 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const dataDirectory = path.join(__dirname, '..', 'DB');
+// استفاده از مسیر موقت Vercel برای نوشتن
+const dataDirectory = path.join('/tmp', 'DB');
 
 const readData = async (filename) => {
   try {
-    const data = await fs.readFile(path.join(dataDirectory, filename), 'utf-8');
+    const filePath = path.join(dataDirectory, filename);
+    const data = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(data);
   } catch (error) {
-    // If file doesn't exist, return an empty array
     if (error.code === 'ENOENT') return [];
     throw error;
   }
@@ -16,23 +17,21 @@ const readData = async (filename) => {
 
 const writeData = async (filename, data) => {
   try {
-    await fs.writeFile(
-      path.join(dataDirectory, filename),
-      JSON.stringify(data, null, 2),
-      'utf-8'
-    );
+    // اطمینان از ایجاد پوشه در مسیر tmp
+    await fs.mkdir(dataDirectory, { recursive: true });
+
+    const filePath = path.join(dataDirectory, filename);
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
   } catch (error) {
     throw error;
   }
 };
-
 
 // Find an item by a specific key-value pair
 const findByKey = async (filename, key, value) => {
   const data = await readData(filename);
   return data.filter(item => item[key] === value);
 };
-
 
 module.exports = {
   readData,
